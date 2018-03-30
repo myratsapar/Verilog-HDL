ec4a57b3cb36db952e3980a1e9fe3df28b4ae52d
Sigmah <saparmyrat.myradov@bil.omu.edu.tr>
2018-03-31 00:06:26 +0300

    Write b in seven segment display

---
+++ sevenbv.v
@@ -0,0 +1,13 @@
+module sevenb(HEX0); //b
+output [6:0] HEX0;
+
+assign HEX0[0] = 1;
+assign HEX0[1] = 1;
+assign HEX0[2] = 0;
+assign HEX0[3] = 0;
+assign HEX0[4] = 0;
+assign HEX0[5] = 0;
+assign HEX0[6] = 0;
+
+endmodule//7:33 PM 30-Mar-18
+
